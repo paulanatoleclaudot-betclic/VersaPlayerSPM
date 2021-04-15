3dc24a3b69195e3108d91f9ee9e9ad676f41166d
Paul-Anatole CLAUDOT <p.claudot@betclicgroup.com>
2021-04-15 10:19:58 +0200

    Rename package to avoid confict

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "VersaPlayer",
+    name: "VersaPlayerSPM",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "VersaPlayer",
-            targets: ["VersaPlayer"]),
+            name: "VersaPlayerSPM",
+            targets: ["VersaPlayerSPM"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "VersaPlayer",
+            name: "VersaPlayerSPM",
             dependencies: [],
             resources: [
                 .process("Resources/Artboard.png"),
